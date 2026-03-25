c6023864e51b502b0286102fb9409576b3152a52
Андрей <alekseev.andrey2512@gmail.com>
2026-03-25 16:27:47 +0300

    A minor correction has been made

--- internal/repositories/whitelist.go
+++ internal/repositories/whitelist.go
@@ -22,11 +22,10 @@ type whitelistRepo struct {
 func (w *whitelistRepo) Del(ctx context.Context, jti string) error {
 	err := w.redisClient.Del(ctx, w.prefix+jti).Err()
 	if err != nil {
-		return fmt.Errorf("failed to del in whitelist %w", err)
+		return fmt.Errorf("failed to del in whitelist: %w", err)
 	}
 	return nil
 }
-
 func (w *whitelistRepo) ConsumeAndAddToken(ctx context.Context, jti, newJti string, exp time.Duration) error {
 	script := redis.NewScript(
 		`local exists = redis.call("EXISTS", KEYS[1])
@@ -38,7 +37,6 @@ func (w *whitelistRepo) ConsumeAndAddToken(ctx context.Context, jti, newJti stri
 				return 0
 			end`,
 	)
-
 	res, err := script.Run(ctx, w.redisClient, []string{w.prefix + jti, w.prefix + newJti}, "true", exp.Milliseconds()).Int()
 
 	if err != nil {
@@ -54,7 +52,7 @@ func (w *whitelistRepo) ConsumeAndAddToken(ctx context.Context, jti, newJti stri
 func (w *whitelistRepo) Add(ctx context.Context, jti string, exp time.Duration) error {
 	err := w.redisClient.Set(ctx, w.prefix+jti, "true", exp).Err()
 	if err != nil {
-		return fmt.Errorf("failed to add to whitelist")
+		return fmt.Errorf("failed to add to whitelist: %w", err)
 	}
 
 	return nil
